19018d062b95ff69ad285140d728bb8a93988188
Casey Schneider-Mizell <caseysm@gmail.com>
2024-03-07 13:16:06 -1000

    generalize file load from cloud

--- src/skel_features/io_utils.py
+++ src/skel_features/io_utils.py
@@ -1,26 +1,43 @@
-import sys
 import json
+import sys
 from pathlib import Path
+
 import pandas as pd
+from cloudpathlib import AnyPath, GSClient, S3Client
+from google.cloud import storage
 from meshparty import meshwork
+
 from . import skel_filtering as filtering
 
 path = Path(__file__)
 sys.path.append(str(path.absolute().parent))
 
+# Configure cloudpathlib to use non-token based clients for public buckets
+storage_client = storage.Client.create_anonymous_client()
+GSClient(storage_client=storage_client).set_as_default_client()
+S3Client(no_sign_request=True).set_as_default_client()
 
 def load_root_id(oid, nrn_dir, peel_threshold=0.1):
     "Load and apply dendrite labels to a neuron based on pre-classified synapse apical labels and more"
-    nrn = meshwork.load_meshwork(f"{nrn_dir}/{oid}.h5")
+    cpath = AnyPath(nrn_dir)
+    if isinstance(oid, str):
+        filename = oid
+    else:
+        filename = f"{oid}.h5"
+    with open(cpath / filename, "rb") as f:
+        nrn = meshwork.load_meshwork(f)
+        nrn.reset_mask()
     filtering.additional_component_masks(nrn, peel_threshold=peel_threshold)
     return nrn
 
 def load_features(root_ids, feature_dir):
     "Load a feature dataframe from a directory"
     dats = []
+    dpath = AnyPath(feature_dir)
     for root_id in root_ids:
+        filename = f"{root_id}.json"
         try:
-            with open(f"{feature_dir}/{root_id}.json") as f:
+            with open(dpath / filename, "r") as f:
                 dats.append(json.load(f))
         except:
             continue
